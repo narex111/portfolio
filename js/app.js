516a0b1d32962b64d02c1e9271b99f73f9522d13
Name Khachatryan <narex111@gmail.com>
2020-01-19 12:16:29 -0800

    links and planets transform respective scales

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
 function mouseOverCheckers(){
     document.getElementById("earth").style.width = "34px";
     document.getElementById("earth").style.height = "34px";
-    console.log("chekcers hovered")
+    document.getElementById("checkers").style.transform = "scale(2)";
 }
 
 function mouseOutCheckers(){
     document.getElementById("earth").style.width = "13px";
     document.getElementById("earth").style.height = "13px";
-    console.log("chekcers outhovered");
+    document.getElementById("checkers").style.transform = "scale(1)";
 }
 
 function mouseOverAboutMe(){
     document.getElementById("sun").style.width = "80px";
     document.getElementById("sun").style.height = "80px";
+    document.getElementById("aboutMe").style.transform = "scale(2)";
 }
 function mouseOutAboutMe(){
     document.getElementById("sun").style.width = "45px";
     document.getElementById("sun").style.height = "45px";
+    document.getElementById("aboutMe").style.transform = "scale(1)";
 }
-
-console.log("js connected")
