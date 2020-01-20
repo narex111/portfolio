6a7d285be154db37a53234b500b0aa6a6bfd35a8
Name Khachatryan <narex111@gmail.com>
2020-01-19 17:10:16 -0800

    venus-movies hover

--- js/app.js
+++ js/app.js
@@ -1,40 +1,74 @@
 //
-//UI Transformation upon HOVER
+//UI Transformation upon Hover
 //
 
 //Home Hover
 function mouseOverHome(){
-    document.getElementById("home").style.transform = "scale(2)";
+    const home = document.getElementById("home")
+
+    home.style.transform = "scale(2)";
 }
 function mouseOutHome(){
-    document.getElementById("home").style.transform = "scale(1)";
+    const home = document.getElementById("home")
+
+    home.style.transform = "scale(1)";
 }
 
 //AboutMe Hover
 function mouseOverAboutMe(){
-    document.getElementById("sun").style.width = "80px";
-    document.getElementById("sun").style.height = "80px";
-    document.getElementById("aboutMe").style.transform = "scale(2)";
+    const sun = document.getElementById("sun")
+    const aboutMe = document.getElementById("aboutMe")
+
+    sun.style.width = "21vmin";
+    sun.style.height = "21vmin";
+    aboutMe.style.transform = "scale(2)";
 }
 function mouseOutAboutMe(){
-    document.getElementById("sun").style.width = "45px";
-    document.getElementById("sun").style.height = "45px";
-    document.getElementById("aboutMe").style.transform = "scale(1)";
+    const sun = document.getElementById("sun")
+    const aboutMe = document.getElementById("aboutMe")
+
+    sun.style.width = "8vmin";
+    sun.style.height = "8vmin";
+    aboutMe.style.transform = "scale(1)";
 }
 
 //Checkers Hover
 function mouseOverCheckers(){
-    document.getElementById("earth").style.width = "34px";
-    document.getElementById("earth").style.height = "34px";
-    document.getElementById("checkers").style.transform = "scale(2)";
+    const earth = document.getElementById("earth")
+    const checkers = document.getElementById("checkers")
+
+    earth.style.width = "13vmin";
+    earth.style.height = "13vmin";
+    checkers.style.transform = "scale(2)";
 }
 
 function mouseOutCheckers(){
-    document.getElementById("earth").style.width = "13px";
-    document.getElementById("earth").style.height = "13px";
-    document.getElementById("checkers").style.transform = "scale(1)";
+    const earth = document.getElementById("earth")
+    const checkers = document.getElementById("checkers")
+
+    earth.style.width = "5vmin";
+    earth.style.height = "5vmin";
+    checkers.style.transform = "scale(1)";
 }
 
+//Movies Hover
+function mouseOverMovies(){
+    const venus = document.getElementById("venus")
+    const movies = document.getElementById("movies")
+
+    venus.style.width = "13vmin";
+    venus.style.height = "13vmin";
+    movies.style.transform = "scale(2)";
+}
+
+function mouseOutMovies(){
+    const venus = document.getElementById("venus")
+    const movies = document.getElementById("movies")
+
+    venus.style.width = "5vmin";
+    venus.style.height = "5vmin";
+    movies.style.transform = "scale(1)";
+}
 
 //
 //UI transformation upon OnClick
